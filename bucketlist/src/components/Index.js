547969d765b624a91dbef6a063c994386708a99a
Jay Glickman <jayglickman@gmail.com>
2020-04-05 13:32:03 -0400

    added Update functionality

--- bucketlist/src/components/Index.js
+++ bucketlist/src/components/Index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Show from "../components/Show";
 import NewForm from "./NewForm";
+import Update from "../components/Update"
 
 let baseURL = 'http://localhost:3003'
 
@@ -23,7 +24,8 @@ class Index extends React.Component {
     currentUser:this.props.currentUser,
     display: false,
     items: [],
-    item: ""
+    item: "",
+    editClicked: false
   }
 
   getItem = item => {
@@ -64,6 +66,20 @@ class Index extends React.Component {
     )
   }
 
+  toggleEditClick = (bool) =>{
+    console.log("editClicked state changed")
+    //if param is givin match state to param otherwise toggle state
+    if (bool){
+      this.setState({
+        editClicked: bool
+      })
+    } else {
+      this.setState({
+        editClicked: !this.state.editClicked
+      })
+    }
+  }
+
   render() {
 
     return (
@@ -86,9 +102,23 @@ class Index extends React.Component {
             onCloseRequest={this.toggleModal}
             display={this.state.display}
             item={this.state.item}
+            toggleEditClick={this.toggleEditClick}
           />
+         {/*Edit form to only be shown when edit button is clicked*/}
+        <div>
+        {this.state.editClicked
+        ? <Update 
+            display={true}
+            item={this.state.item}
+            toggleEditClick={this.toggleEditClick}
+          />
+        : null
+          }
+        </div>
           {this.props.currentUser ? <h1>Logged In</h1> : null}
+          
       </div>
+
     );
   }
 
